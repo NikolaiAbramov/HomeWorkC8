b172d9b529130099fed69b7c8f89dce5ed3109c9
NikolaiAbramov <abramovn85@mail.ru>
2023-04-03 21:55:53 +0700

    Task62 done

---
+++ Task62/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+int[,] matrix = new int[4, 4];
+int count = 1;
+int minRow = 0, maxRow = 3, minCol = 0, maxCol = 3;
+
+while (count <= 16)
+{
+    for (int i = minCol; i <= maxCol; i++)
+    {
+        matrix[minRow, i] = count++;
+    }
+
+    for (int i = minRow + 1; i <= maxRow; i++)
+    {
+        matrix[i, maxCol] = count++;
+    }
+
+    for (int i = maxCol - 1; i >= minCol; i--)
+    {
+        matrix[maxRow, i] = count++;
+    }
+
+    for (int i = maxRow - 1; i > minRow; i--)
+    {
+        matrix[i, minCol] = count++;
+    }
+
+    minRow++;
+    minCol++;
+    maxRow--;
+    maxCol--;
+}
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{matrix[i, j]:00} \t");
+    }
+    Console.WriteLine();
+}
